9b7740c40a5784b56059ee9a1a721aa1079598ae
Viktor Poliushko <milleniiumkb@gmail.com>
2020-04-04 22:37:42 +0300

    data transformation

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -7,11 +7,7 @@ export default class PlanetDetails extends Component {
   swapiService = new SwapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null,
+    planet: {}
   };
 
   constructor() {
@@ -19,21 +15,17 @@ export default class PlanetDetails extends Component {
     this.updatePlanet();
   }
 
+  onPlanetLoaded = planet => {
+    this.setState({planet});
+  }
+
   updatePlanet() {
     const id = Math.floor(Math.random()* 19) + 2;
-    this.swapiService.getPlanet(id).then((planet) => {
-      this.setState({
-        id,
-        name: planet.name,
-        population: planet.population,
-        rotationPeriod: planet.rotation_period,
-        diameter: planet.diameter,
-      });
-    });
+    this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
   }
 
   render() {
-    const { id, name, population, rotationPeriod, diameter } = this.state;
+    const { planet: { id, name, population, rotationPeriod, diameter } } = this.state;
 
     return (
       <div className="random-planet jumbotron rounded">
