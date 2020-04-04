2c12fd537b24dd8f0b7082385393483163e606ab
Viktor Poliushko <milleniiumkb@gmail.com>
2020-04-05 00:19:34 +0300

    spinned added

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner";
 
 import "./random-planet.css";
 
@@ -29,27 +30,7 @@ export default class PlanetDetails extends Component {
 
     return (
       <div className="random-planet jumbotron rounded">
-        <img
-          className="planet-image"
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-        />
-        <div className="random-planet__info">
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
+        <Spinner />
       </div>
     );
   }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./spinner.css";
+
+export default class Spinner extends Component {
+  render() {
+    return (
+      <div className="loadingio-spinner-disk-wlv147dlq7">
+        <div className="ldio-ou8padd06r">
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
