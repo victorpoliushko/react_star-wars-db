d04e37bace5e582188d7f6089bd36f1445c5479d
Viktor Poliushko <milleniiumkb@gmail.com>
2020-04-28 17:10:19 +0300

    show people

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,7 @@ import "./app.css";
 export default class App extends Component {
   state = {
     showRandomPlanet: true,
+    selectedPerson: null,
   };
 
   onTogglePlanet = () => {
@@ -16,6 +17,12 @@ export default class App extends Component {
       return {
         showRandomPlanet: !state.showRandomPlanet,
       };
+    });
+  };
+
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
     });
   };
 
@@ -34,10 +41,10 @@ export default class App extends Component {
         </button>
         <div className="row mb2">
           <div className="col-md-6">
-            <ItemList />
+            <ItemList onItemSelected={this.onPersonSelected} />
           </div>
           <div className="col-md-6">
-            <PersonDetails />
+            <PersonDetails personId={this.state.selectedPerson} />
           </div>
         </div>
       </div>
